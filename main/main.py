7335a45b0229db2a8b66f98de9a9b243ad885df8
lucidcreeper <2010030071@klh.edu.in>
2022-03-13 22:04:00 +0530

    commit

--- main/main.py
+++ main/main.py
@@ -38,22 +38,12 @@ def dijkstra(graph, start_vertex):
                         D[neighbor] = new_cost
     return D
 
-g = Graph(9)
-g.add_edge(0, 1, 4)
-g.add_edge(0, 6, 7)
-g.add_edge(1, 6, 11)
-g.add_edge(1, 7, 20)
-g.add_edge(1, 2, 9)
-g.add_edge(2, 3, 6)
-g.add_edge(2, 4, 2)
-g.add_edge(3, 4, 10)
-g.add_edge(3, 5, 5)
-g.add_edge(4, 5, 15)
-g.add_edge(4, 7, 1)
-g.add_edge(4, 8, 5)
-g.add_edge(5, 8, 12)
-g.add_edge(6, 7, 1)
-g.add_edge(7, 8, 3) 
+g = Graph(4)
+g.add_edge(0, 2, 3)
+g.add_edge(0, 3, 3)
+g.add_edge(1, 2, 3)
+g.add_edge(2, 3, 2)
+g.add_edge(3, 1, 7)
 
 @app.route("/")
 def index():
@@ -72,7 +62,7 @@ def saveDetails():
             D = dijkstra(g, int(start))
             msg = ""
             for vertex in range(len(D)):
-                msg = msg + "Distance from vertex 0 to vertex"+str(vertex)+"is"+str(D[vertex])+"\n\n\n"
+                msg = msg + "Distance from vertex 0 to vertex"+str(vertex)+"is"+str(D[vertex]) 
         except:
             msg = "counldnt start"
         finally:
